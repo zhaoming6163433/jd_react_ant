07553648f974810b287b3441decb98f206103596
xiongcui <xiongcui@jd.com>
2019-09-10 16:11:48 +0800

    文件创建

---
+++ src/page/aa.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button,LocaleProvider, Layout } from 'antd';
+
+class aa extends Component {
+    render() {
+      return (
+        <div className="App">
+          <Button type="primary">Button</Button>
+        </div>
+      );
+    }
+  }
+
+export default aa;
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import aa from '../page/aa'
+const routes = [
+  {
+    exact: true,
+    path: '/',
+    component: aa
+  },
+  {
+    exact: true,
+    path: '/a',
+    component: aa
+  }
+];
+
+export default routes;
