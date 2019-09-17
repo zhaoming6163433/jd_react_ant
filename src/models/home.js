16f7b9ba6173ca1ea0774a6d1cb606abf8aaac13
JDJR\zhaoming16 <zhaoming16@jd.com>
2019-09-17 11:28:15 +0800

    增加表格过滤

--- src/models/home.js
+++ src/models/home.js
@@ -1,17 +1,35 @@
 import { post_user_info } from '@/services/api';
+import React from 'react';
 
 export const home = {
   state: {
-    columns: [{
-          title: '姓名',
-          dataIndex: 'name',
-        }, {
-          title: 'ID',
-          dataIndex: 'id',
-    }],
+    columns: [
+        {
+            title: '姓名',
+            dataIndex: 'name',
+            key: 'name'
+        },
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id'
+        },
+        {
+            title: '年龄',
+            dataIndex: 'age',
+            key: 'age',
+            render: (text) => {
+                const hide = (text)=>{
+                    return text+'岁'
+                }
+                return (<a>{hide(text)}</a>)
+            }
+        }
+    ],
     userinfo: [{
         "name": "赵明",
-        "id": "122"
+        "id": "122",
+        "age": 18
     }]
   },
 
